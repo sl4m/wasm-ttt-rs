9678e112c99b65c4cd35c84961deede9252f20de
skim <skim@swainslane.co>
2020-06-23 18:01:40 -0700

    integrates game with web/js

---
+++ www/index.js
@@ -0,0 +1,35 @@
+import { Game } from "wasm-ttt-rs";
+
+const DRAW_MESSAGE = "The game is a draw";
+const WIN_MESSAGE = "The winner is ";
+const HUMAN_MARK = "X";
+const CPU_MARK = "O";
+const DISABLE_CLICK = " disable-click";
+const game = Game.with_defaults();
+const cells = document.getElementsByClassName("cell");
+
+Array.from(cells).forEach(cell => {
+  cell.addEventListener("click", e => {
+    const el = e.toElement;
+    el.className += DISABLE_CLICK;
+    el.innerText = HUMAN_MARK;
+    const cpuMove = game.play_round(+el.dataset.pos);
+
+    if (cpuMove && !game.is_over()) {
+      const cpuEl = document.querySelector('[data-pos="' + cpuMove + '"]');
+      cpuEl.className += DISABLE_CLICK;
+      cpuEl.innerText = CPU_MARK;
+    }
+
+    if (game.is_over()) {
+      const winner = game.winner();
+      const msgEl = document.getElementById("message");
+
+      if (winner) {
+        msgEl.innerText = WIN_MESSAGE + winner;
+      } else {
+        msgEl.innerText = DRAW_MESSAGE;
+      }
+    }
+  });
+});
